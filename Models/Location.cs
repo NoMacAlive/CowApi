f11492e617b5270882c9ef419a27d08d5d3d9a1d
Guangya Zhu <guangya.zhu@re-leased.com>
2022-04-21 21:00:16 +1200

    Created response object for get cows api call

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
 namespace HalterExercise.Models
 {
     public class Location
     {
+        [JsonPropertyName("lat")]
         public int Latitude { get; set; }
+        [JsonPropertyName("long")]
         public int Longitude { get; set; }
     }
 }
---
+++ Models/RequestModels/GetCowsResponse.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace HalterExercise.Models.RequestModels
+{
+	public class GetCowsResponse
+	{
+		[JsonPropertyName("id")]
+		public Guid Id { get; set; }
+		[JsonPropertyName("collarId")]
+		public int CollarId { get; set; }
+		[JsonPropertyName("cowNumber")]
+		public int CowNumber { get; set; }
+		[JsonPropertyName("lastLocation")]
+		public Location LastLocation { get; set; }
+	}
+}
